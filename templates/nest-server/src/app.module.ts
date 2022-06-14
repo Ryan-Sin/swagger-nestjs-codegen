f7205acd7d3afd4ce25785de492a2a35472c1037
RyanSin <ryansin937@gmail.com>
2022-06-14 18:26:04 +0900

    feat: 1.1.1 config 환경 설정

--- templates/nest-server/src/app.module.ts
+++ templates/nest-server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 {{#each modules}} 
 import { {{domainNameClass}}Module} from './module/{{domainFrom}}.module';
 {{/each}}
 
 @Module({
-  imports: [{{#each modules}} {{domainNameClass}}Module, {{/each}}],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${__dirname}/../src/config/.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    {{#each modules}} {{domainNameClass}}Module, {{/each}}],
   controllers: [],
   providers: [],
 })
