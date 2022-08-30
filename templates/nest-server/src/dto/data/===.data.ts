46dfa9431e878ae406d3943fcca5e2d6b2863206
RyanSin <ryansin937@gmail.com>
2022-08-30 19:00:35 +0900

    feat: object 타입 및 array 타입 $ref가 아닌 타입 사용 설정

--- templates/nest-server/src/dto/data/===.data.ts
+++ templates/nest-server/src/dto/data/===.data.ts
@@ -8,7 +8,7 @@ export class {{className}}Data {
      @ApiProperty({
        description: '{{{this.variableDescription}}}',
        required: {{this.variableRequired}},
-      {{#if this.varibaleExample}}example: {{#exampleCheck this.varibaleExample }} '{{this}}'{{/exampleCheck}} {{/if}}
+      {{#if this.varibaleExample}}example: {{#typeCheck this.varibaleExample }} '{{this}}'{{/typeCheck}} {{/if}}
     })    
     {{#each this.variableClassValidator}}
     {{this}}
