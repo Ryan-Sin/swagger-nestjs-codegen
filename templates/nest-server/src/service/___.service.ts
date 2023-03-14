c3219876175c23f1d1f2bd8626cbff27740940c1
RyanSin <ryansin937@gmail.com>
2023-03-14 16:03:07 +0900

    feat: service Reqeust 정보 추가

--- templates/nest-server/src/service/___.service.ts
+++ templates/nest-server/src/service/___.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 {{#each serviceImportRequestDto}}import { {{this.className}} } from  {{#typeCheck this.from }} {{this}} {{/typeCheck}};{{/each}}
 
 @Injectable()
 export class {{domainName}}Service {
 {{#each router}}
-  {{methodName}}({{#each serviceParam}}{{this.variableName}} : {{this.variableType}},{{/each}}) : Promise<any> {
+  {{methodName}}({{#each serviceParam}}{{this.variableName}} : {{this.variableType}},{{/each} req: Request}) : Promise<any> {
     return new Promise((resolve, reject) => {
       resolve({});
     });
